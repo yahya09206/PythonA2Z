10e927c75df10847908e532cfe538c4435a074b3
Yahya <yahyahussein91@gmail.com>
2025-07-20 02:54:16 -0700

    printing 5 in a row

---
+++ A2Z/Chapter2/star_square.py
@@ -0,0 +1,14 @@
+"""
+ * Write for loops to produce the following output:
+ *
+ * *****
+ * *****
+ * *****
+ * *****
+ *
+"""
+
+for i in range(1, 5):
+    for j in range(1, 6):
+        print("*", end='')
+    print()
