ad93c48fccb4c8053a243bd953caede2e10d2628
Yahya <yahyahussein91@gmail.com>
2025-07-20 02:34:14 -0700

    corrected for loop

--- A2Z/Chapter2/wave_numbers40.py
+++ A2Z/Chapter2/wave_numbers40.py
@@ -7,6 +7,21 @@
  * 1122334455667788990011223344556677889900
  * ----------------------------------------
  *
+ for(int j = 0; j < 2; j++) {
+    for(int i = 1; i <= 10; i++) {
+        System.out.print(i%10);
+        System.out.print(i%10);
+    }
+}
 """
-for i in range(1,41):
+for i in range(1, 41):
     print("-", end='')
+
+print()
+for i in range(1, 11):
+    print("_-^-", end='')
+print()
+for i in range(0, 2):
+    for j in range(1,11):
+        print(j%10, end='')
+        print(j%10, end='')
