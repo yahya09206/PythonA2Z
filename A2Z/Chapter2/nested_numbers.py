5c77fe199d5856b54cdb7f65dc3b89dcafacc743
Yahya <yahyahussein91@gmail.com>
2025-07-20 03:15:51 -0700

    printing 3 rows

---
+++ A2Z/Chapter2/nested_numbers.py
@@ -0,0 +1,25 @@
+"""
+Write nested for loops that produce the following output:
+000111222333444555666777888999
+000111222333444555666777888999
+000111222333444555666777888999
+for(int i = 1; i <= 3; i++){
+			for(int j = 0; j < 10; j++){
+				for(int k = j; k <= j; k++){
+					System.out.print(k%10);
+					System.out.print(j);
+				}
+				System.out.print(j);
+			}
+			System.out.println();
+
+"""
+
+# print 3 rows of numbers
+for i in range(0, 3):
+    for j in range(0, 10):
+        for k in range(j, j):
+            print(k%10)
+            print(j)
+        print(j, end='')
+    print()
