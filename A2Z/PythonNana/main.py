32982576e4e3c59a8285dbe61fe7c5d75e983057
Yahya <yahyahussein91@gmail.com>
2025-07-26 09:17:47 -0700

    added check for specific type of units

--- A2Z/PythonNana/main.py
+++ A2Z/PythonNana/main.py
@@ -1,16 +1,21 @@
-to_seconds = 24
-name_of_units = "hours"
+# to_seconds = 24
+# name_of_units = "hours"
 
-def days_to_units(num_of_days: int):
-    return f"{num_of_days} days are {num_of_days * to_seconds} {name_of_units}"
+def days_to_units(num_of_days: int, conversion_unit):
+    if conversion_unit == "hours":
+        return f"{num_of_days} days is {num_of_days * 24} hours}"
+    elif conversion_unit == "minutes":
+        return f"{num_of_days} days is {num_of_days * 24 * 60} minutes"
+    else:
+        return "unsupported unit"
 
 # function for valid input check
 def validate_and_execute():
     try:
 
-            user_input_number = int(num_of_days_elements)
+            user_input_number = int(days_and_unit_dictionary["days"])
             if user_input_number > 0:
-                calculated_value = days_to_units(user_input_number)
+                calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
                 print(calculated_value)
             elif user_input_number == 0:
                 print("you entered a 0, please enter a valid positive number")
@@ -22,10 +27,9 @@ def validate_and_execute():
 user_input = ""
 # accept user input
 while user_input != "exit":
-    user_input = input("Hey user, enter a number of days and I will convert it to seconds!\n")
-    list_of_days = user_input.split(", ")
-    for num_of_days_elements in set(list_of_days):
-        validate_and_execute()
-
-
-
+    user_input = input("Hey user, enter a number of days and conversion unit!\n")
+    days_and_unit = user_input.split(":")
+    print(days_and_unit)
+    days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
+    print(days_and_unit_dictionary)
+    validate_and_execute()
