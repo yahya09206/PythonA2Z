3aecaea138bea4a72717eeb475bbfac4d9245a78
Yahya <yahyahussein91@gmail.com>
2025-07-26 09:26:42 -0700

    making program modular

---
+++ A2Z/PythonNana/helper.py
@@ -0,0 +1,22 @@
+def days_to_units(num_of_days: int, conversion_unit):
+    if conversion_unit == "hours":
+        return f"{num_of_days} days is {num_of_days * 24} hours"
+    elif conversion_unit == "minutes":
+        return f"{num_of_days} days is {num_of_days * 24 * 60} minutes"
+    else:
+        return "unsupported unit"
+
+# function for valid input check
+def validate_and_execute(days_and_unit_dictionary):
+    try:
+
+            user_input_number = int(days_and_unit_dictionary["days"])
+            if user_input_number > 0:
+                calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
+                print(calculated_value)
+            elif user_input_number == 0:
+                print("you entered a 0, please enter a valid positive number")
+            else:
+                print("you entered a negative number: please enter a positive number")
+    except ValueError:
+            print("your input is not a valid number, Don't ruin my program!")
--- A2Z/PythonNana/main.py
+++ A2Z/PythonNana/main.py
@@ -1,28 +1,4 @@
-# to_seconds = 24
-# name_of_units = "hours"
-
-def days_to_units(num_of_days: int, conversion_unit):
-    if conversion_unit == "hours":
-        return f"{num_of_days} days is {num_of_days * 24} hours"
-    elif conversion_unit == "minutes":
-        return f"{num_of_days} days is {num_of_days * 24 * 60} minutes"
-    else:
-        return "unsupported unit"
-
-# function for valid input check
-def validate_and_execute():
-    try:
-
-            user_input_number = int(days_and_unit_dictionary["days"])
-            if user_input_number > 0:
-                calculated_value = days_to_units(user_input_number, days_and_unit_dictionary["unit"])
-                print(calculated_value)
-            elif user_input_number == 0:
-                print("you entered a 0, please enter a valid positive number")
-            else:
-                print("you entered a negative number: please enter a positive number")
-    except ValueError:
-            print("your input is not a valid number, Don't ruin my program!")
+from helper import validate_and_execute
 
 user_input = ""
 # accept user input
@@ -32,4 +8,5 @@ while user_input != "exit":
     print(days_and_unit)
     days_and_unit_dictionary = {"days": days_and_unit[0], "unit": days_and_unit[1]}
     print(days_and_unit_dictionary)
-    validate_and_execute()
+    validate_and_execute(days_and_unit_dictionary)
+
