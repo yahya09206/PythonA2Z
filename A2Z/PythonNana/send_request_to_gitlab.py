f0e102c12fe27d8529b09fdd265e9e200a71695c
Yahya <yahyahussein91@gmail.com>
2025-07-26 10:11:55 -0700

    sending requests to retrieve json info

---
+++ A2Z/PythonNana/send_request_to_gitlab.py
@@ -0,0 +1,8 @@
+import requests
+
+response = requests.get("https://gitlab.com/api/v4/users/nanuchi/projects")
+my_projects = response.json()
+
+for project in my_projects:
+    print(f"Project Name: {project['name']} Project Url: {project['web_url']}")
+
