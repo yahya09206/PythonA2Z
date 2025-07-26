027328c40a6fb90245cbfd64e667bc8845f4eccf
Yahya <yahyahussein91@gmail.com>
2025-07-26 02:35:59 -0700

    removed duplicated code

--- A2Z/PythonNana/main.py
+++ A2Z/PythonNana/main.py
@@ -2,19 +2,17 @@ to_seconds = 24
 name_of_units = "hours"
 
 def days_to_units(num_of_days: int):
-    if num_of_days > 0:
-        return f"{num_of_days} days are {num_of_days * to_seconds} {name_of_units}"
-    elif num_of_days == 0:
-        return "you entered a 0, please enter a valid positive number"
-    else:
-        return "you entered a negative value, so no conversion for you"
+    return f"{num_of_days} days are {num_of_days * to_seconds} {name_of_units}"
 
 # function for valid input check
 def validate_and_execute():
     if user_input.isdigit():
         user_input_number = int(user_input)
-        calculated_value = days_to_units(user_input_number)
-        print(calculated_value)
+        if user_input_number > 0:
+            calculated_value = days_to_units(user_input_number)
+            print(calculated_value)
+        elif user_input_number == 0:
+            print("you entered a 0, pleaser enter a valid positive number")
     else:
         print("your input is not a valid number, Don't ruin my program!")
 
