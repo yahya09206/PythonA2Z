88c95a36bdd867d0e685f320a7ffbc7578efdc21
Yahya <yahyahussein91@gmail.com>
2025-07-26 09:44:04 -0700

    verified output

--- A2Z/PythonNana/time_until_deadline.py
+++ A2Z/PythonNana/time_until_deadline.py
@@ -6,5 +6,9 @@ input_list = user_input.split(":")
 goal = input_list[0]
 deadline = input_list[1]
 
-datetime.datetime.strptime(deadline, "d.m.y")
-print(input_list)
+deadline_date = datetime.datetime.strptime(deadline, "%d.%m.%Y")
+today_date = datetime.datetime.today()
+time_till = deadline_date - today_date
+
+# calculate how many days from now until the deadline
+print(f"Dear user! Time remaining for your goal: {goal} is {time_till.days} days")
