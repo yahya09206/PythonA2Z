e3b7ea16876adc29ffbf1374869d0b02951bbe3c
Yahya <yahyahussein91@gmail.com>
2025-07-26 09:36:45 -0700

    formatting date entered

--- A2Z/PythonNana/time_until_deadline.py
+++ A2Z/PythonNana/time_until_deadline.py
@@ -1,1 +1,10 @@
 import datetime
+
+user_input = input("enter your goal with a deadline separated by a colon\n")
+input_list = user_input.split(":")
+
+goal = input_list[0]
+deadline = input_list[1]
+
+datetime.datetime.strptime(deadline, "d.m.y")
+print(input_list)
