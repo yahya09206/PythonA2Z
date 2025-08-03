6a2fcc91acf18b62303d7cb2a3403c8ff84a189c
Yahya <yahyahussein91@gmail.com>
2025-08-03 02:09:28 -0700

    checking length and if something is there

--- A2Z/NeetCode/Lists/check_list.py
+++ A2Z/NeetCode/Lists/check_list.py
@@ -1,5 +1,15 @@
 def check_list_empty(my_list) -> bool:
-    if len(my_list) > 0:
-        print("The list is not empty")
-    else:
-        print("The list is empty")
+    return len(my_list) == 0
+
+
+def check_element_in_list(my_list, element) -> bool:
+    return element in my_list
+
+print(check_list_empty([]))
+print(check_list_empty([1, 2, 3]))
+
+print(check_element_in_list([1, 2, 3], 1))
+print(check_element_in_list([1, 2, 3], 4))
+
+print(check_element_in_list(["Apple", "Banana", "Orange"], "Banana"))
+print(check_element_in_list(["Apple", "Banana", "Orange"], "Grape"))
