6e2872997a0e33fef69d0f7709da020a9c7ff65c
yahya09206 <yahyahussein91@gmail.com>
2025-08-07 18:58:40 -0700

    moving zeroes to end of list

---
+++ Leetcode/two_pointer/move_zereos.py
@@ -0,0 +1,13 @@
+from typing import List
+def move_zeroes_to_right(nums):
+
+    left = 0
+    for right in range(len(nums)):
+        if nums[right] != 0:
+            nums[left], nums[right] = nums[right], nums[left]
+            left += 1
+
+
+nums = [1, 0, 2, 0, 3, 4, 0, 5, 6]
+move_zeroes_to_right(nums)
+print(nums)
