a64e24dd81df3dcf6a782d38a41b18fdcf69677b
Yahya <yahyahussein91@gmail.com>
2025-08-16 02:14:20 -0700

    swapping indices using temp value

--- Leetcode/two_pointer/reverse_array.py
+++ Leetcode/two_pointer/reverse_array.py
@@ -1,10 +1,18 @@
 from typing import List
 
-def reverse_list(arr: List[int]) -> None:
+def reverse_list(arr: List[int]) -> List:
 
     # initialize a left and right pointer
     l = 0
     r = len(arr) - 1
 
+    while l < r:
+        temp = arr[l]
+        arr[l] = arr[r]
+        arr[r] = temp
+        l += 1
+        r -= 1
+    return arr
 
 
+print(reverse_list([1, 2, 3, 4, 5]))
