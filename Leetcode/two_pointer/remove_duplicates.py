be35bc035a43cbd470d7bf329e1c851e5074a6ae
Yahya <yahyahussein91@gmail.com>
2025-08-16 04:19:49 -0700

    removed duplicates

--- Leetcode/two_pointer/remove_duplicates.py
+++ Leetcode/two_pointer/remove_duplicates.py
@@ -2,3 +2,13 @@ from typing import List
 
 
 def removed_duplicates(nums: List[int]) -> int:
+
+    l = 0
+
+    for r in range(len(nums)):
+        if nums[r] != nums[l]:
+            nums[l + 1] = nums[r]
+            l += 1
+    return l + 1
+
+print(removed_duplicates([0,0,1,1,1,2,2,3,3,4]))
