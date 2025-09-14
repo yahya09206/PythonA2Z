aca091a4063c9ddfdb59ed6bed81c0b468171978
Yahya <yahyahussein91@gmail.com>
2025-09-14 00:54:51 -0700

    setting up for code

---
+++ Leetcode/two_pointer/container_with_most_water.py
@@ -0,0 +1,18 @@
+"""
+Need to find which heights from left to right have the most water based on width and height
+
+result = 0
+init left and right pointer
+
+while l < r:
+compute the area:
+area = (r-l) * min(height[l], height[r])
+
+increment left pointer if height[l] is greater than height[r]
+
+else decrement right
+
+return res
+"""
+from typing import List
+def container_with_most(height: List[int]) -> int:
