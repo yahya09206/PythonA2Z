2e64afcdf7103e3d325103695bb243f81584f7e7
Yahya <yahyahussein91@gmail.com>
2025-09-15 22:49:54 -0700

    incremented l by one and returned to see number of uniques

--- Leetcode/two_pointer/remove_duplicates_from_sorted_array.py
+++ Leetcode/two_pointer/remove_duplicates_from_sorted_array.py
@@ -7,6 +7,8 @@ def remove_duplicates_from_sorted_array(nums: List[int]) -> int:
     for r in range(1, len(nums)):
         if nums[r] != nums[r - 1]:
             nums[l] = nums[r]
+            l += 1
+        return l
 
 
 
