c1f7b81a4fd7a27931cafadad41c6eb24a83bd59
Yahya <yahyahussein91@gmail.com>
2025-09-16 17:58:40 -0700

    ran into some more hiccups

--- Leetcode/revision/reverse_string_part_two.py
+++ Leetcode/revision/reverse_string_part_two.py
@@ -1,17 +1,15 @@
 from typing import List
 
-def reverse_string(s: List[str]) -> None:
+def reverse_string_part_two(s: List[str]):
 
     l = 0
     r = len(s) - 1
 
     while l < r:
-        temp = s[l]
-        s[l] = s[r]
-        s[r] = temp
+        s[l], s[r] = s[r], s[l]
         l += 1
         r -= 1
 
 s = ["w", "a", "k", "e"]
-reverse_string(s)
+reverse_string_part_two(s)
 print(s)
