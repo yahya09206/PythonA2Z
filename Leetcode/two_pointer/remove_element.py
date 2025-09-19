88c7bba2b417f13e45dcf4f8a440a237d4db8c31
Yahya <yahyahussein91@gmail.com>
2025-09-18 17:49:54 -0700

    changed condition to reflect target value

--- Leetcode/two_pointer/remove_element.py
+++ Leetcode/two_pointer/remove_element.py
@@ -6,9 +6,13 @@ def remove_element(nums: List[int], target: int) -> int:
 
     for r in range(len(nums)):
 
-        if nums[r] == target:
+        if nums[r] != target:
             temp = nums[l]
             nums[l] = nums[r]
             nums[r] = temp
             l += 1
     return l
+
+
+print(remove_element([3, 2, 2, 3, 3, 4], 2))
+print(remove_element([0,1,2,2,3,0,4,2], 2))
