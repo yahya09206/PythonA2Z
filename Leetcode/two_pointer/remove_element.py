9e5e77e7f21a165c11820e8478235c7d6c11845d
Yahya <yahyahussein91@gmail.com>
2025-09-18 17:47:48 -0700

    added return statement and value to be returned

--- Leetcode/two_pointer/remove_element.py
+++ Leetcode/two_pointer/remove_element.py
@@ -1,13 +1,14 @@
 from typing import List
 
-def remove_element(nums: List[int], target: int) -> None:
+def remove_element(nums: List[int], target: int) -> int:
 
     l = 0
 
     for r in range(len(nums)):
 
-        if nums[r]:
+        if nums[r] == target:
             temp = nums[l]
             nums[l] = nums[r]
             nums[r] = temp
             l += 1
+    return l
