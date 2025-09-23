84895c6dc041ec751c4d82dad29539018e7c8e06
Yahya <yahyahussein91@gmail.com>
2025-09-23 12:54:26 -0700

    fixed location of array

--- Leetcode/two_pointer/three_sum.py
+++ Leetcode/two_pointer/three_sum.py
@@ -11,11 +11,11 @@ def three_sum(nums: List[int]) -> List[List[int]]:
     result = []
 
     for i in range(len(nums)):
-        # break out of loop if first number is greater than 0
+        # break out of loop if the first number is greater than 0
         if nums[i] > 0:
             break
         # continue loop if the previous value is the same as current value r > 0 and nums[r] == nums[r - 1]
-        elif r > 0 and nums[i] == nums[i - 1]:
+        elif i > 0 and nums[i] == nums[i - 1]:
             continue
 
         # set l pointer to next index or i in for loop
@@ -45,7 +45,8 @@ def three_sum(nums: List[int]) -> List[List[int]]:
                 l += 1
             else:
                 r -= 1
-        return result
+    # return result array
+    return result
 
 
 
