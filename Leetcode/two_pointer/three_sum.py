09567e2bc1efe195d5b6b6ebc924893add8f1a6b
Yahya <yahyahussein91@gmail.com>
2025-09-23 12:38:16 -0700

    set loop to continue if values are the same

--- Leetcode/two_pointer/three_sum.py
+++ Leetcode/two_pointer/three_sum.py
@@ -11,8 +11,11 @@ def three_sum(nums: List[int]) -> List[List[int]]:
     result = []
 
     for r in range(len(nums)):
-
+        # break out of loop if nums[r] > 0
         if nums[r] > 0:
             break
+        elif r > 0 and nums[r] == nums[r - 1]:
+            continue
+
 
 
