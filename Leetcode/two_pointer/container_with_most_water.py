1d8da8fb682394ef19fde7f05e6377d9b99d430c
Yahya <yahyahussein91@gmail.com>
2025-09-23 13:52:25 -0700

    added clarifying comments to container with most water code

--- Leetcode/two_pointer/container_with_most_water.py
+++ Leetcode/two_pointer/container_with_most_water.py
@@ -14,22 +14,41 @@ increment left pointer if height[l] is <= than height[r]
 else decrement right
 
 return res
+
+
+The key insight: We always move the pointer at the shorter wall because:
+
+The water level is always limited by the shorter wall
+Moving the taller wall's pointer would only make the width smaller with no chance of improvement
+Moving the shorter wall's pointer might find a taller wall, giving us a chance for more water
 """
 from typing import List
 def container_with_most(height: List[int]) -> int:
 
+    # result to keep track of the current biggest area
     res = 0
     l = 0
     r = len(height) - 1
 
+
     while l < r:
+        # Calculate the water area between our two "walls":
+        # min(height[l], height[r]) = height (water level is limited by the shorter wall)
+        # Example: If l=1, r=8, height[1]=8, height[8]=7, then area = (8-1) * min(8,7) = 7 * 7 = 49
         area = (r - l) * min(height[l], height[r])
+        # Update our best result. If this new area is bigger than what we had before, keep it.
+        # Otherwise, stick with the old best.
         res = max(res, area)
 
+        # If the left wall is shorter or equal, move the left pointer to the right.
+        # Why? The shorter wall is the "bottleneck" - we want to find a potentially taller left wall.
         if height[l] <= height[r]:
             l += 1
+        # If the right wall is shorter, move the right pointer to the left.
+        # Same logic - we're looking for a potentially taller right wall.
         else:
             r -= 1
+    # Return the maximum area we found
     return res
 
 
--- Leetcode/two_pointer/three_sum.py
+++ Leetcode/two_pointer/three_sum.py
@@ -11,41 +11,46 @@ def three_sum(nums: List[int]) -> List[List[int]]:
     result = []
 
     for i in range(len(nums)):
-        # break out of loop if the first number is greater than 0
+        # break out of loop if the first number is greater than zero which will mean the rest are higher since the array is sorted
+        # so none of the indexes will equal to zero
         if nums[i] > 0:
             break
-        # continue loop if the previous value is the same as current value r > 0 and nums[r] == nums[r - 1]
+        # continue aka skip loop if the previous value is the same as current value r > 0 and nums[r] == nums[r - 1]
+        # this prevents duplicate triplets
         elif i > 0 and nums[i] == nums[i - 1]:
             continue
 
-        # set l pointer to next index or i in for loop
+        # set l pointer to start after current number located in nums[i]
         l = i + 1
         # set an r pointer to the end of array
         r = n
 
-        # start while loop
+        # start while loop and keep going as long as the left pointer is to the left of the right pointer
         while l < r:
             # calculate current sum nums[i] + nums[l] + nums[r]
             sum = nums[i] + nums[l] + nums[r]
 
-            # if sum is equal to 0 then append the values to the result array [nums[i], nums[l], nums[r]]
+            # if sum is equal to 0 then append the values to the result array [[nums[i], nums[l], nums[r]]
+            # we found our triplets
             if sum == 0:
                 result.append([nums[i], nums[l], nums[r]])
-                # move pointers
+                # move pointers inward to look for more triplets
                 l += 1
                 r -= 1
                 # move the left pointer by one if the previous is the same value so while l < r and nums[l] == nums[l - 1]
+                # essentially skipping duplicate numbers on the left side
                 while l < r and nums[l] == nums[l - 1]:
                     l += 1
-                # check the same for the right pointer
+                # check the same for the right pointer and skip duplicates on the right side
                 while l < r and nums[r] == nums[r + 1]:
                     r -= 1
-            # if sum is less than zero, move either pointer to get a bigger sum
+            # if the sum is less than zero, move the left pointer to get a bigger sum
             elif sum < 0:
                 l += 1
+            # if the sum is greater than zero, move the right pointer to get a smaller sum
             else:
                 r -= 1
-    # return result array
+    # return result array and give back all the triplets that were found
     return result
 
 
