52c3759d74623b09c39165046faf24c4238b5d42
Yahya <yahyahussein91@gmail.com>
2025-09-25 17:27:04 -0700

    created empty set

--- Leetcode/sliding_window/longest_substring_without_repeats.py
+++ Leetcode/sliding_window/longest_substring_without_repeats.py
@@ -1,3 +1,7 @@
 from typing import List
 
 def longest_substring_without_repeating(s: str) -> int:
+
+    # LINE 1: Create empty set to track characters in the current window, this will store
+    # we've seen in our current window
+    charSet = set()
