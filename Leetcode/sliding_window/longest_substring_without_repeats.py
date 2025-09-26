a731d509fdb93dc8337f9fe8ab551e20276d159e
Yahya <yahyahussein91@gmail.com>
2025-09-25 17:38:06 -0700

    going into loop

--- Leetcode/sliding_window/longest_substring_without_repeats.py
+++ Leetcode/sliding_window/longest_substring_without_repeats.py
@@ -6,8 +6,15 @@ def longest_substring_without_repeating(s: str) -> int:
     # A set automatically prevents duplicates.
     charSet = 0
 
+    # Sets up our left pointer at the beginning of the string (index 0). This marks the start of our current substring.
     l = 0
+
+    # Initialize our result to 0. This will track the longest substring length we've found so far.
     result = 0
+
+    # Start a loop where r (right pointer) goes through each character in the string from left to right.
+    # This marks the end of our current substring.
+    for r in range(len(s)):
 
 
 
