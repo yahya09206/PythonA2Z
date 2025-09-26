c485cf9152f89de45aa6199a77f717b53b5ed56d
Yahya <yahyahussein91@gmail.com>
2025-09-25 17:37:09 -0700

    provided clearer comments

--- Leetcode/sliding_window/longest_substring_without_repeats.py
+++ Leetcode/sliding_window/longest_substring_without_repeats.py
@@ -2,15 +2,12 @@ from typing import List
 
 def longest_substring_without_repeating(s: str) -> int:
 
-    # LINE 1: Create empty set to track characters in the current window, this will store characters
-    # we've seen in our current window
-    charSet = set()
-
-    """
-    Set left pointer to 0 (start of string)
-    Set result to 0 (maximum length found so far)
-    """
+    # Creates an empty set to keep track of characters we've already seen in our current "window".
+    # A set automatically prevents duplicates.
+    charSet = 0
 
     l = 0
     result = 0
+
+
 
