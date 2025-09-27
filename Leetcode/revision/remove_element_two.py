3516b5d649045fc4eee989334605f2328f24ec9b
Yahya <yahyahussein91@gmail.com>
2025-09-27 00:38:01 -0700

    verified logic is working

--- Leetcode/revision/remove_element_two.py
+++ Leetcode/revision/remove_element_two.py
@@ -2,3 +2,16 @@ from typing import List
 
 def remove_element_two(nums: List[int], target: int) -> int:
 
+    l = 0
+
+    for r in range(len(nums)):
+        if nums[r] != target:
+            temp = nums[l]
+            nums[l] = nums[r]
+            nums[r] = temp
+            l += 1
+    return l
+
+
+print(remove_element_two([1, 2, 3, 4], 2))
+
