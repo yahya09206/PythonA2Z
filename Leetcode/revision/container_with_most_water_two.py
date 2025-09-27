c1479161e82a5fde61dd97df0b79d393588a6e02
Yahya <yahyahussein91@gmail.com>
2025-09-26 17:36:59 -0700

    moving pointer based on which side of the wall is smaller

--- Leetcode/revision/container_with_most_water_two.py
+++ Leetcode/revision/container_with_most_water_two.py
@@ -10,5 +10,11 @@ def container_two(height: List[int]) -> int:
     while l < r:
 
         area = (l - r) * min(height[l], height[r])
+        result = max(result, area)
+
+        if height[l] <= height[r]:
+            l += 1
+        else:
+            r -= 1
 
 
