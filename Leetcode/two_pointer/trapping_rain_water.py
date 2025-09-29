9411f7c79adf2b3891710012e4766b7d81d50b42
Yahya <yahyahussein91@gmail.com>
2025-09-29 09:39:30 -0700

    keeping track of left and right maxes

--- Leetcode/two_pointer/trapping_rain_water.py
+++ Leetcode/two_pointer/trapping_rain_water.py
@@ -8,6 +8,15 @@ def trapping_rain_water(height: List[int]) -> int:
     l = 0
     r = len(height) - 1
 
+    left_max = height[l]
+    right_max = height[r]
+
+    result = 0
+
+
+
+
+
 
 
     return result
