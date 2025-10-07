0354a03f9d3e4e992dd74f3cd8430f9e5e1d053a
Yahya <yahyahussein91@gmail.com>
2025-10-06 22:12:16 -0700

    finished with reverse three

--- Leetcode/sliding_window/reverse_string_three.py
+++ Leetcode/sliding_window/reverse_string_three.py
@@ -1,4 +1,18 @@
 from typing import List
 
-def reverse_string_three(s: List[str]) -> None:
+def reverse_string_three(s: List[str]):
+
+    l = 0
+    r = len(s) - 1
+
+    while l < r:
+        temp = s[l]
+        s[l] = s[r]
+        s[r] = temp
+        l += 1
+        r -= 1
+
+word = ['w', 'a', 'k', 'e']
+reverse_string_three(word)
+print(word)
 
